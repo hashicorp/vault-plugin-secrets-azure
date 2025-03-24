bff134d0367d4cbbdd072440f3176396fd179bce
Jaired Jawed <jaired.jawed@hashicorp.com>
2025-03-24 10:08:57 -0700

    Backport change from #233 into version 1.16.x

--- path_roles.go
+++ path_roles.go
@@ -320,7 +320,7 @@ func (b *azureSecretBackend) pathRoleUpdate(ctx context.Context, req *logical.Re
 			roleDef = *defs[0]
 		}
 		roleDefID := *roleDef.ID
-		roleDefName := *roleDef.Name
+		roleDefName := *roleDef.Properties.RoleName
 
 		r.RoleName, r.RoleID = roleDefName, roleDefID
 
